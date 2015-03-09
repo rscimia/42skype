c2ddde75f538fdb8c5f53b41b3075cb7006f8e17
Romain <romain@dhcp-9-43.citt-drire.emn.fr>
2015-03-09 15:46:14 +0100

    récupération corps

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ spark.on('login', function() {
 
 	http.get("http://mystatus.skype.com/savnika.num", function(res) {
 	  	console.log("Got response: " + res.statusCode);
+	  	res.on('data', function (chunk) {
+		    console.log('BODY: ' + chunk);
+		});
 	}).on('error', function(e) {
 	  	console.log("Got error: " + e.message);
 	});
