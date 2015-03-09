8092cb506b0daf6f95a9258963a5bb3989d1306e
Romain <romain@dhcp-9-43.citt-drire.emn.fr>
2015-03-09 15:58:24 +0100

    ajout boucle

--- server.js
+++ server.js
@@ -4,20 +4,25 @@
 var spark = require('spark');
 var http = require('http');
 
-spark.on('login', function() {
-  	// If login is successful we get and accessToken,
-  	// we'll use that to call Spark API ListDevices
-
-  	var devicesPr = spark.listDevices();
-
+var checkStatus = function(){
 	http.get("http://mystatus.skype.com/savnika.num", function(res) {
 	  	console.log("Got response: " + res.statusCode);
 	  	res.on('data', function (chunk) {
 		    console.log('BODY: ' + chunk);
+		    spark.login({ username: 'romain@42factory.com', password: '42spark' }, sendSpark(chunk));
 		});
 	}).on('error', function(e) {
 	  	console.log("Got error: " + e.message);
 	});
+
+	setTimeout(checkStatus, 2000);
+}
+
+var sendSpark = function(statusCode) {
+  	// If login is successful we get and accessToken,
+  	// we'll use that to call Spark API ListDevices
+
+  	var devicesPr = spark.listDevices();
 
   	devicesPr.then(
 	    // We get an array with devices back and we list them
@@ -33,16 +38,23 @@ spark.on('login', function() {
 	        	}
 	     	};
 
-	      	// The function needs to be defined  in the firmware uploaded to the
-	      	// the Spark core and registered to the Spark cloud, same thing we do
-	      	// with variables. You pass along the name of the function and the params.
-	      	spark.callFunction(devices[0].id, 'color', '50', callback);
-
-	      	// Once you hvae a device/core instance you can use that to call functions on it.
+	      	// Once you have a device/core instance you can use that to call functions on it.
 	      	// The main difference between this and directly using the main `spark` instance is
 	      	// that you no longer need to pass the id.
 	      	var core = devices[0];
-	      	core.callFunction('color', '120', callback);
+
+	      	switch(statusCode){
+	      	case 0:
+				core.callFunction('color', '0', callback);
+			case 1:
+				core.callFunction('color', '40', callback);
+			case 2:
+				core.callFunction('color', '80', callback);
+			case 3:
+				core.callFunction('color', '120', callback);
+			case 4:
+				core.callFunction('color', '160', callback);
+	      	}
 	    },
 	    function(err) {
 	    	console.log('API call failed: ', err);
@@ -50,6 +62,5 @@ spark.on('login', function() {
 	);
 });
 
-// Login as usual
-spark.login({ username: 'romain@42factory.com', password: '42spark' });
+checkStatus();
 
